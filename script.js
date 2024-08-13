2c79febc0b802e294c821044d4da739d80d28d58
Nick Rabone <n.rabone72@aol.com>
2024-08-13 04:27:21 +0100

    crested scene and cube mesh

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 import * as THREE from 'three'
 
-// const scene = new THREE.Scene()
+const scene = new THREE.Scene()
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xff4500
+})
 
-console.log(THREE)
+const cubeMesh = new THREE.Mesh(
+  cubeGeometry,
+  cubeMaterial
+)
+
+console.log(cubeMesh)
+console.log(scene)
+ 
